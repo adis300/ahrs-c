6aa84d02c1f7fcfb613427a87d9d83ac364cddd8
Disi A <adis@live.cn>
2021-07-01 22:47:24 +0800

    Update to use rad instead of degree.

--- arhs.h
+++ arhs.h
@@ -25,12 +25,12 @@
 
 // https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
 #define COMPUTE_EULER_ANGLE(WS) \
-	WS->yaw = ATAN2(2 * WS->q0 * WS->q3 + 2 * WS->q1 * WS->q2, 1 - 2 * WS->q2 * WS->q2 - 2 * WS->q3 * WS->q3) * 180/2; \
+	WS->yaw = ATAN2(2 * WS->q0 * WS->q3 + 2 * WS->q1 * WS->q2, 1 - 2 * WS->q2 * WS->q2 - 2 * WS->q3 * WS->q3); \
 	MA_PRECISION sinp = 2 * WS->q0 * WS->q2 - 2 * WS->q1 * WS->q3;\
-	if(sinp > M_PI/2) WS->pitch = 180/2;\
-	else if(sinp < -M_PI/2) WS->pitch = -180/2;\
-	else WS->pitch = ASIN(sinp) * 180/M_PI;\
-	WS->roll = ATAN2(2 * WS->q0 * WS->q1 + 2 * WS->q2 * WS->q3, 1 -2 * WS->q1 * WS->q1 - 2 * WS->q2 * WS->q2)*180/M_PI
+	if(sinp > M_PI/2) WS->pitch = M_PI/2;\
+	else if(sinp < -M_PI/2) WS->pitch = -M_PI/2;\
+	else WS->pitch = ASIN(sinp);\
+	WS->roll = ATAN2(2 * WS->q0 * WS->q1 + 2 * WS->q2 * WS->q3, 1 -2 * WS->q1 * WS->q1 - 2 * WS->q2 * WS->q2)
 
 
 // See: http://en.wikipedia.org/wiki/Fast_inverse_square_root
